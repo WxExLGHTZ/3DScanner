909fc6fb8804f191cf19d60dc1be247bdbbde3bb
Mert Karadeniz <mert.karadeniz@student.htw-berlin.de>
2022-07-05 12:15:11 +0200

    minor fix arduino_Portcheck

--- Software/Backend_Pakete/arduino_Portcheck.py
+++ Software/Backend_Pakete/arduino_Portcheck.py
@@ -2,9 +2,19 @@ import serial.tools.list_ports
 
 def check_arduino_connection():
     ports = list(serial.tools.list_ports.comports())
-    if ports:
-        print("Arduino verbunden")
-        return True
-    else:
-        print("Arduino nicht verbunden")
-        return False
+
+    string = "USB"
+
+    for p in ports:
+
+        check = str(p).find(string)
+
+        if check == -1:
+            print("Arduino nicht verbunden")
+
+        else:
+            print("Arduino verbunden")
+
+
+
+
